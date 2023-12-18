a8e80c1762d4399fda44638e5dc78fb1a130eff7
Divith Siddu <divithsiddu17@gmail.com>
2023-12-18 15:01:42 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from reviewprediction import logger
-logger.info("coustom logger test")
+from reviewprediction.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
